e66c89fd40c75f5d23e2cc96cebbcf9e80ebdce2
Lhg2323 <1558000617@qq.com>
2025-04-02 20:54:17 +0800

    commit

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import { useAuthStore } from '@/stores/auth'
+import router from '@/router'
 
 const request = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
@@ -25,6 +26,9 @@ request.interceptors.request.use(
 request.interceptors.response.use(
   response => {
     const res = response.data
+    if (res.code === 4001) {
+       router.push('/login')
+    }
     if (res.code !== 200) {
       ElMessage.error(res.message || '请求失败')
       return Promise.reject(new Error(res.message || '请求失败'))
