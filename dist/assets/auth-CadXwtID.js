41d44c4ae1d3b0cc6840535fa61d308a149323a6
Lhg2323 <1558000617@qq.com>
2025-04-08 02:50:11 +0800

    commit

---
+++ dist/assets/auth-CadXwtID.js
@@ -0,0 +1,1 @@
+import{r as e}from"./request-tfwnRnxz.js";const o={login(r){return e({url:"/app/user/login",method:"post",data:r})},sendEmailCode(r){return e({url:"/app/user/sendEmailCode",method:"post",data:r})},register(r){return e({url:"/app/user/register",method:"post",data:r})},handleForget(r){return e({url:"/app/user/forgetPassword",method:"post",data:r})}};export{o as a};
